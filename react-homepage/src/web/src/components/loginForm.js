c49ed0dc20339a9ed383b7281692afe9f5bb3c7a
gee05053 <gee05053@gmail.com>
2023-04-15 03:13:29 +0900

    modify style

--- react-homepage/src/web/src/components/loginForm.js
+++ react-homepage/src/web/src/components/loginForm.js
@@ -2,21 +2,30 @@ import { Button, Card, Input, Row, Col } from 'antd';
 
 function LoginForm() {
   return(
-    <Card>
-      <Row>
-        <Col span={17}>
-          <Input placeholder='군번' style={{ width:180 }}/>
-          <Input.Password placeholder='육군 이메일 비밀번호' style={{ width:180 }}/>
+    <Card style={{width: "96%"}}>
+      <Row gutter={[20,60]}>
+        <Col span={16}>
+          <Input placeholder='군번' style={{ width:170 }}/>
+          <Input.Password placeholder='육군 이메일 비밀번호' style={{ width:170 }}/>
         </Col>
-        <Col>
+        <Col span={8}>
           <Button style={{ height: 64}}>
               로그인
           </Button>
         </Col>
       </Row>
-      <Button type="link" size='small'>
-          비밀번호 초기화
-      </Button>
+      <Row gutter={25}>
+        <Col span={15}>
+          <Button type="link" size='small'>
+              비밀번호 초기화
+          </Button>
+        </Col>
+        <Col span={9}>
+          <Button type="link" size='small'>
+              사용자 등록
+          </Button>
+        </Col>
+      </Row>
     </Card>
   );
 }
