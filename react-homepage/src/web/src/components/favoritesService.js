70d87e76740efc7aca204560fbdec0dbb4c6470f
gee05053 <gee05053@gmail.com>
2023-04-15 01:33:58 +0900

    complete facoritesService

---
+++ react-homepage/src/web/src/components/favoritesService.js
@@ -0,0 +1,70 @@
+import { Badge, Button, Card, Row, Col } from 'antd';
+
+function FavoritesService () {
+  return (
+    <Card size="small" title="자주찾는서비스">
+      <Row>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="지휘통제"/>
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="부대발전제언"/>
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="일정관리"/>
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="일일상황평가회의"/>
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="전투일일결산"/>
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="초과근무관리"/>
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="부대행정업무"/>
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="위병소출입관리"/>
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="보안일일결산"/>
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="link">
+            <Badge status="default" text="배차일지"/>
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+}
+
+export default FavoritesService;
--- react-homepage/src/web/src/pages/homepage.js
+++ react-homepage/src/web/src/pages/homepage.js
@@ -2,6 +2,7 @@ import { Row, Col } from 'antd';
 import Header from '../components/header';
 import Navbar from '../components/navbar';
 import LoginForm from '../components/loginForm';
+import FavoritesService from '../components/favoritesService';
 
 function Homepage() {
   return(
@@ -17,6 +18,33 @@ function Homepage() {
           <Row>
             <Col span={5}>
               <LoginForm/>
+            </Col>
+            <Col span={12}>
+              여단 배경
+            </Col>
+            <Col span={7}>
+              여단 배너
+            </Col>
+          </Row>
+        </Col>
+        <Col span={2}/>
+        {"\u00A0"}
+      </Row>
+      <Row>
+        <Col span={2}/>
+        <Col span={20}>
+          <Row>
+            <Col span={5}>
+              <FavoritesService/>
+            </Col>
+            <Col span={12}>
+              공지사항
+            </Col>
+            <Col span={7}>
+              날씨
+              사이버대응태세
+              배너1
+              배너2
             </Col>
           </Row>
         </Col>
