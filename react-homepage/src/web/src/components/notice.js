353cf91812d5a51a790b85277956d00982023f09
gee05053 <gee05053@gmail.com>
2023-07-06 17:59:07 +0900

    add notice

--- react-homepage/src/web/src/components/notice.js
+++ react-homepage/src/web/src/components/notice.js
@@ -1,11 +1,27 @@
-import { Tabs } from 'antd';
+import { Row, Col, Tabs } from 'antd';
+import engNotice from './json/notice.json';
 
 function Notice () {
   const tabList = [
     {
       key: "공지사항",
       label: "공지사항",
-      children: "no 공지사항",
+      children: 
+        engNotice.data.map((data) => {
+          return(
+            <Row>
+              <Col span={3}>
+                {"["+data.dept+"]"}
+              </Col>
+              <Col span={18}>
+                {data.title}
+              </Col>
+              <Col>
+                {data.date}
+              </Col>
+            </Row>
+          );
+        })
     },
     {
       key: "5군단",
